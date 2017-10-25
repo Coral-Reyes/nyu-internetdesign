2c4ff691fba8d3c13456b2097943d42a893d5e87
Coral Reyes <koralflor11@gmail.com>
2017-10-25 21:07:18 +0000

    Added Firebase

--- JavaScript/content.js
+++ JavaScript/content.js
@@ -1,3 +1,14 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAGrv7Q2mJ7DBNHIOKMdVSS7-eo6muJ_uw",
+    authDomain: "my-portfolio-studio.firebaseapp.com",
+    databaseURL: "https://my-portfolio-studio.firebaseio.com",
+    projectId: "my-portfolio-studio",
+    storageBucket: "my-portfolio-studio.appspot.com",
+    messagingSenderId: "458669713089"
+  };
+  firebase.initializeApp(config);
 $(".grid").masonry({
   // options...
   itemSelector: '.grid-item' ;
---
+++ JavaScript/script.js
@@ -0,0 +1,11 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAGrv7Q2mJ7DBNHIOKMdVSS7-eo6muJ_uw",
+    authDomain: "my-portfolio-studio.firebaseapp.com",
+    databaseURL: "https://my-portfolio-studio.firebaseio.com",
+    projectId: "my-portfolio-studio",
+    storageBucket: "my-portfolio-studio.appspot.com",
+    messagingSenderId: "458669713089"
+  };
+  firebase.initializeApp(config);
